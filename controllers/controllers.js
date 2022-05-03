52d382deccee22179012b173253c1f13ded2d037
Mauricio Hunau <maurih@live.com>
2022-05-03 18:12:23 -0300

    fixing problems

--- controllers/controllers.js
+++ controllers/controllers.js
@@ -4,20 +4,21 @@ const router = Router();
 router.get('/', (req, res)=>{
     const max = 1001
     const min = 1
-    const number = Math.floor(Math.random() * (max - min) + min);
+    let number = Math.floor(Math.random() * (max - min) + min);
+
     if(number === 1 && number <= 100){
-        console.log(number)
-        return res.status (204), console.log("Legendary")
+        //Legendary
+        return res.status(200).json(number) 
     }else if(number >= 101 && number <= 250){
-        console.log(number)
-        return res.status (204), console.log("Epic") 
+        //Epic
+        return res.status(200).json(number) 
     }else if(number >= 251 && number <= 500){
-        console.log(number)
-        return res.status (204), console.log("Rare") 
+        //Rare
+        return res.status(200).json(number) 
     }else if(number >= 501 && number <= 1000){
-        console.log(number)
-        return res.status (204), console.log("Common") 
+        //Common
+        return res.status(200).json(number)  
     }
 })
 
-module.exports = router
+module.exports = router;
